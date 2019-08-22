9cd83346d1e31c5cd4748fa223d7c0dc863d679f
Daniel Segall <segalld@gmail.com>
2019-08-22 16:13:24 +0100

    Finish task 12

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -11,13 +11,18 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
+    subject.dock(Bike.new)
     bike = subject.release_bike
     expect(bike).to be_working
   end
 
-  it 'does it store a docked bike?' do
+  it 'stores a docked bike' do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+  end
+
+  it 'throws an error if there is no bike in the docking station' do
+    expect { subject.release_bike }.to raise_error 'No bikes available'
   end
 end
